b316819940d511a4f4758a19d05ba0c78a0d57a5
Brian <nguyenvanquyet@lgcns.com>
2025-12-02 09:29:55 +0700

    [feat]:Setting up Project & Fetching and Adding new Categories

---
+++ src/main/java/com/ecommerce/sb_ecom/model/Category.java
@@ -0,0 +1,15 @@
+package com.ecommerce.sb_ecom.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Category {
+    private Long categoryId;
+    private String categoryName;
+}
